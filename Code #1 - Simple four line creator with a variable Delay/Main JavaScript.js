70a21f0aa037fb1773d62da3bab70795f1a28aea
enppp <94575200+enppp@users.noreply.github.com>
2026-03-12 08:45:09 -0500

    Save #1

---
+++ Code #1 - Simple four line creator with a variable Delay/Main JavaScript.js
@@ -0,0 +1,10 @@
+function delay() {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+async function createLines() {
+  let variableNameHere = document.getElementById("TEXT-CONTAINER-NAME-HERE")
+
+  for (let i = 0,
+}
